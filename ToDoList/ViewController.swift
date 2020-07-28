17524b9740e6678d3ac809299addc2e6b6491c6d
Isabel Ung <itssewisabel@gmail.com>
2020-07-28 10:21:11 -0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Isabel Ung on 7/28/20.
+//  Copyright © 2020 Isabel Ung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
